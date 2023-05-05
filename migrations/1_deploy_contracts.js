f4f23662e3462dab35305e50a73607ae75025f1b
Preston-Sundar <sundar.preston.789@gmail.com>
2023-05-04 21:35:15 -0500

    Add abi loading

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -5,6 +5,8 @@ const Simple_Storage = artifacts.require("SimpleStorage")
 module.exports = function(deployer) {
   // deployer.deploy(Insurace_Policy, "0x91d1368fd9c618c0df8522f6f082c8a5c7794167",
   // 1, 1)
-  deployer.deploy(Insurace_Factory, ["0x91d1368fd9c618c0df8522f6f082c8a5c7794167"], [30])
-  deployer.deploy(Insurace_Policy, 1, "0x62B72B6dC51cD817B3Ea0f050bB4517ADeBACCC1", 31, "0x91d1368fd9c618c0df8522f6f082c8a5c7794167")
+  deployer.deploy(Insurace_Factory, ["0x91d1368fd9c618c0df8522f6f082c8a5c7794167"], 8);
+  deployer.deploy(Insurace_Policy, 1, "0x62B72B6dC51cD817B3Ea0f050bB4517ADeBACCC1", 31, "0x91d1368fd9c618c0df8522f6f082c8a5c7794167");
+
+  deployer.deploy(Simple_Storage);
 };
