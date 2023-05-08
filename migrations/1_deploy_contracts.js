5e76d07d4b1cf9644a11830bd3a212703a5e356b
austinjp17 <apalm35@gmail.com>
2023-05-07 23:47:25 -0500

    Per Vendor haircut

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -4,6 +4,6 @@ const Insurace_Factory = artifacts.require("Insurance_Factory")
 module.exports = function(deployer) {
   // deployer.deploy(Insurace_Policy, "0x91d1368fd9c618c0df8522f6f082c8a5c7794167",
   // 1, 1)
-  deployer.deploy(Insurace_Factory, ["0x91d1368fd9c618c0df8522f6f082c8a5c7794167"], 8)
+  deployer.deploy(Insurace_Factory, ["0x91d1368fd9c618c0df8522f6f082c8a5c7794167"], [30])
   deployer.deploy(Insurace_Policy, 1, "0x62B72B6dC51cD817B3Ea0f050bB4517ADeBACCC1", 31, "0x91d1368fd9c618c0df8522f6f082c8a5c7794167")
 };
